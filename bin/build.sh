2dea05946da521ef7cb1a23ff2236dc78d7d1249
edison <edisonlil@163.com>
2025-08-22 10:33:04 +0800

    build: 修改 git 分支处理逻辑
    - 移除了对分支名称进行裁剪的代码
    -现在使用完整的分支名称进行 git clone 操作

--- bin/build.sh
+++ bin/build.sh
@@ -170,8 +170,7 @@ function scm() {
 		#克隆代码
 		if test -n "${opt_git_branch}" ; then
 			info "开始使用git拉取代码,当前分支:${opt_git_branch}"
-			#处理存在orgin/test的问题
-			real_branch=${opt_git_branch##*/}
+			real_branch=${opt_git_branch}
 			echo "埋点:git的real_branch:$real_branch"
 			git clone -b  ${real_branch}  --single-branch $opt_git_url  $cfg_temp_dir
 		else
